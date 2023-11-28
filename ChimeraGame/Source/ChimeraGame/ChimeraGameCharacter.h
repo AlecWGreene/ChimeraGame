2c7f90cab62ecba4ab895e0c9604928500c2d9bd
Alec W Greene <alecwgreene@gmail.com>
2023-11-28 09:24:16 -0800

    Added default mesh to BP_Humanoid

--- ChimeraGame/Source/ChimeraGame/ChimeraGameCharacter.h
+++ ChimeraGame/Source/ChimeraGame/ChimeraGameCharacter.h
@@ -16,28 +16,6 @@ public:
 
 	virtual const class UChimeraAnimSet* GetAnimSetForMesh(const USkeletalMeshComponent* InMesh) const override;
 
-	//----- Components -----//
-protected:
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Mesh)
-	TObjectPtr<USkeletalMeshComponent> WeaponMesh_Left;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Mesh)
-	TObjectPtr<USkeletalMeshComponent> WeaponMesh_Right;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Mesh)
-	TObjectPtr<USkeletalMeshComponent> ShieldMesh_Left;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Mesh)
-	TObjectPtr<USkeletalMeshComponent> ShieldMesh_Right;
-
-public:
-
-	USkeletalMeshComponent* GetWeaponMesh_Left() const { return WeaponMesh_Left; }
-	USkeletalMeshComponent* GetWeaponMesh_Right() const { return WeaponMesh_Right; }
-	USkeletalMeshComponent* GetShieldMesh_Left() const { return ShieldMesh_Left; }
-	USkeletalMeshComponent* GetShieldMesh_Right() const { return ShieldMesh_Right; }
-
 	//----- Class Settings -----//
 public:
 
